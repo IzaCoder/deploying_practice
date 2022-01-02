12626ef2a26e1c0c1159fd25ed6da0289b489355
IzaCoder <izacoder@icloud.com>
2022-01-02 00:37:21 +0000

    Add routes

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// set up homepage route
+app.get("/", (req, res) => {
+    res.send("This is the homepage");
+});
+
+// set up second page
+app.get("/second", (req, res) => {
+    res.send("This is the second page");
+});
+
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
 });
