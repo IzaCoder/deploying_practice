fe5bd811651eac5609759420be01cf035ac90909
IzaCoder <izacoder@icloud.com>
2022-01-02 00:52:50 +0000

    Update to move routing to routes.js

--- server.js
+++ server.js
@@ -4,16 +4,7 @@ const app = express();
 const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// set up homepage route
-app.get("/", (req, res) => {
-    res.send("This is the homepage");
-});
-
-// set up second page
-app.get("/second", (req, res) => {
-    res.send("This is the second page");
-});
+app.use("/", require("./routes"));
 
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
