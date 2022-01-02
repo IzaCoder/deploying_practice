9c87c0ac386b3d25afbb68201d7292c7f9f74c91
IzaCoder <izacoder@icloud.com>
2022-01-02 00:35:23 +0000

    Add code for using body-parser module

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
